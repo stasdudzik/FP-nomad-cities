0f6e262fe3f865f4bfd739990710259cefa5a1c5
Stas Dudzik <stas.dudzik@gmail.com>
2021-02-18 10:29:41 +0100

    added notes on pure functions

---
+++ 7.PureFunctions.js
@@ -0,0 +1,100 @@
+const R = require("ramda");
+const fs = require("fs");
+const percentile = require("./percentile.js");
+const data = fs.readFileSync("./cities.json", "utf8");
+const cities = JSON.parse(data);
+
+const KtoC = (k) => k - 273.15;
+const KtoF = (k) => (k * 9) / 5 - 459.67;
+const updateTemp = R.curry((convertFn, city) => {
+  const temp = Math.round(convertFn(city.temp));
+  return { ...city, temp };
+  // ramda way
+  // return R.merge(city, { temp });}
+});
+const updtCities = R.map(updateTemp(KtoF), cities);
+// console.log(updtCities);
+
+// currying
+
+const firstCity = cities[0];
+const updatedCity = updateTemp(KtoC, firstCity);
+// console.log(updatedCity);
+
+// we can simplyfy this weird call with Ramda curry function
+// how to disappear the second pair of parents?
+
+const totalCostReducer = (acc, city) => {
+  const { cost = 0 } = city;
+  return acc + cost;
+};
+
+const cityCount = R.length(updtCities);
+const totalCost = R.reduce(totalCostReducer, 0, updtCities);
+// console.log(totalCost / cityCount);
+
+const groupByPropReducer = (acc, city) => {
+  const { cost = [], internetSpeed = [] } = acc;
+  return R.merge(acc, {
+    cost: R.append(city.cost, cost),
+    internetSpeed: R.append(city.internetSpeed, internetSpeed),
+  });
+};
+
+// object for properties : cost and internetspeed
+// using an empty object to create a new object
+
+const groupedByProp = R.reduce(groupByPropReducer, {}, updtCities);
+
+// console.log(groupedByProp);
+
+const calcScore = (city) => {
+  const { cost = 0, internetSpeed = 0 } = city;
+  const costPercentile = percentile(groupedByProp.cost, cost);
+  const internetSpeedPercentile = percentile(
+    groupedByProp.internetSpeed,
+    internetSpeed
+  );
+  const score = 100 * (1.0 - costPercentile) + 20 * internetSpeedPercentile;
+  return R.merge(city, { score });
+};
+
+// take a look at arguments order pattern in ramda
+const scoredCities = R.map(calcScore, updtCities);
+// console.log(scoredCities);
+
+// filter temperature and humidity
+
+const filterByWeather = (city) => {
+  const { temp = 0, humidity = 0 } = city;
+  return temp > 68 && temp < 85 && humidity > 30 && humidity < 70;
+};
+
+const filteredCities = R.filter(filterByWeather, scoredCities);
+
+// console.log(R.length(filteredCities));
+
+const sortedCities = R.sortWith(
+  [R.descend((city) => city.score)],
+  filteredCities
+);
+
+// console.log(sortedCities);
+
+const top10 = R.take(10, sortedCities);
+
+// console.log(top10);
+// console.log(R.length(top10));
+
+console.log("********************");
+
+/* pure functions so far 
+-reusable
+-easy to test
+-easy to cache
+-composable - good to create new functions
+  - composition meaning: composing functions of smaller functions
+  - makePie example
+  - composed functions work from right to left 
+  - pipe reverse that order (why both pipe and compose?)
+*/
